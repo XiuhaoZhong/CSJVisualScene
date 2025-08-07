9a3785ae339bcc5baf906f5f344d680114f53c0f
Mr.DuoReMi <zxhas@foxmail.com>
2025-08-07 23:34:23 +0800

    Update CSJSceneEngine.cpp

--- src/CSJSceneRuntime/CSJSceneEngine.cpp
+++ src/CSJSceneRuntime/CSJSceneEngine.cpp
@@ -7,94 +7,11 @@
 #include <stdexcept>
 #include <vulkan/vulkan_core.h>
 
-// #include "stbi/stb_image.h"
-// #include "tinyobjloader/tiny_obj_loader.h"
-
 #include "Utils/CSJLogger.h"
-
 #include "CSJSceneRuntime/core/math/coordinate_definitions.h"
 #include "CSJSceneRuntime/function/global/CSJRuntimeContext.h"
 
 using namespace CSJEngine;
-
-static const uint32_t MAX_FRAMES_IN_FLIGHT = 2;
-
-const std::string MODEL_PATH = "resources/models/viking_room.obj";
-const std::string TEXTURE_PATH = "resources/textures/viking_room.png";
-
-struct UniformBufferObject {
-    glm::mat4 model;
-    glm::mat4 view;
-    glm::mat4 proj;
-};
-
-struct Vertex {
-    glm::vec3 pos;
-    glm::vec3 color;
-    glm::vec2 texCoord;
-
-    static VkVertexInputBindingDescription getBindingDescription() {
-        VkVertexInputBindingDescription bindingDescription{};
-        bindingDescription.binding = 0;
-        bindingDescription.stride = sizeof(Vertex);
-        bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
-
-        return bindingDescription;
-    }
-
-    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescription() {
-        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions{};
-
-        attributeDescriptions[0].binding = 0;
-        attributeDescriptions[0].location = 0;
-        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[0].offset = offsetof(Vertex, pos);
-
-        attributeDescriptions[1].binding = 0;
-        attributeDescriptions[1].location = 1;
-        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
-        attributeDescriptions[1].offset = offsetof(Vertex, color);
-
-        attributeDescriptions[2].binding = 0;
-        attributeDescriptions[2].location = 2;
-        attributeDescriptions[2].format = VK_FORMAT_R32G32_SFLOAT;
-        attributeDescriptions[2].offset = offsetof(Vertex, texCoord);
-
-        return attributeDescriptions;
-    }
-
-    bool operator==(const Vertex& other) const {
-        return pos == other.pos && color == other.color && texCoord == other.texCoord;
-    }
-};
-
-namespace std {
-    template<> struct hash<Vertex> {
-        size_t operator()(Vertex const& vertex) const {
-            return ((hash<glm::vec3>()(vertex.pos) ^ (hash<glm::vec3>()(vertex.color) << 1)) >> 1) ^ (hash<glm::vec2>()(vertex.texCoord) << 1);
-        }
-    };
-}
-
-const std::vector<Vertex> vertices = {
-    {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-    {{ 0.5f, -0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}},
-    {{ 0.5f,  0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
-    {{-0.5f,  0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}},
-
-    {{-0.5f, -0.5f, -0.5f}, {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f}},
-    {{ 0.5f, -0.5f, -0.5f}, {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f}},
-    {{ 0.5f,  0.5f, -0.5f}, {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f}},
-    {{-0.5f,  0.5f, -0.5f}, {0.0f, 0.0f, 1.0f}, {1.0f, 1.0f}}
-};
-
-const std::vector<uint16_t> indices = {
-    0,1,2,2,3,0,
-    4,5,6,6,7,4
-};
-
-std::vector<Vertex> obj_verteices;
-std::vector<uint32_t> obj_indices;
 
 CSJSceneEngine::CSJSceneEngine(QVulkanWindow* window)
     : m_pWindow(window)
@@ -166,7 +83,9 @@ void CSJSceneEngine::startNextFrame() {
     /* logic tick                                    */
     /* render tick                                   */
     /*************************************************/
-    m_pGlobalContext->getRenderSystem()->render_tick(m_pWindow->currentCommandBuffer(), 0.0, m_pWindow->currentSwapChainImageIndex());
+    m_pGlobalContext->getRenderSystem()->render_tick(m_pWindow->currentCommandBuffer(), 
+                                                     0.0, 
+                                                     m_pWindow->currentSwapChainImageIndex());
 
     m_pWindow->frameReady();
     m_pWindow->requestUpdate();
